fd8cf038a1ab88bc68a7f02c5b85ca6048ada87b
Immanuel Thoke <immanuel@matchwerk.de>
2021-07-16 15:39:15 +0200

    set experimental flag, add longest path, add initial pos, rearrange

--- Sugiyama.py
+++ Sugiyama.py
@@ -3,14 +3,25 @@ import matplotlib.pyplot as plt
 import random
 from remove_cycles import remove_selfloops, greedy_cycle_removal, revert_edges
 from assign_levels import longest_path
+from global_sifting import global_sifting
 
-def sugiyama(DiGraph):
-    remove_selfloops(DiGraph)
-    node_order = greedy_cycle_removal(DiGraph)
-    revert_edges(DiGraph, node_order)
-    return DiGraph
+## EXPERIMENTAL STATE
 
-#g = nx.gn_graph(5) # a tree
-g = nx.scale_free_graph(50)
-nx.draw(g)            
-        
+def init_sugiyama(G):
+    
+    remove_selfloops(G)
+    lvl_dict = longest_path(G)
+    print(lvl_dict)
+
+    # init positions
+    x = 0
+    for node in G.nodes:
+        nx.set_node_attributes(G, { node: x }, 'x')
+        nx.set_node_attributes(G, { node: lvl_dict[node] }, 'y')
+        x += 1 # set to distance finally
+
+
+def run_sugiyama(G):
+    node_order = greedy_cycle_removal(G)
+    revert_edges(G, node_order)
+    return G
