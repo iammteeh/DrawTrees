dad82e8408f1b7a6c30173a6c3a448ec77941ae8
Immanuel Thoke <immanuel@matchwerk.de>
2021-07-22 08:40:55 +0200

    set debug graph

--- main.py
+++ main.py
@@ -64,11 +64,18 @@ def assign_layout(G, graph_type):
 
 def main():
     for filename in graphlist:
+        g = nx.complete_graph(7)
+        g = nx.to_directed(g)
+        G = nx.DiGraph()
+        for node in g.nodes:
+            G.add_node(node)
+        for edge in g.edges:
+            G.add_edge(edge[0],edge[1])
         logging.Formatter('%(asctime)s %(levelname)s %(message)s')
         logging.getLogger('./output/' + filename).setLevel(logging.WARNING)
         path_to_file = filepath + filename
         savefile = './output/' + filename
-        G = parse_input(input_format, path_to_file, multigraph_key)
+        #G = parse_input(input_format, path_to_file, multigraph_key)
         pos = assign_layout(G, graph_type)
         
         # plotting
