b34303dbfcaa9188bf88c58a8d395be23dca4702
Immanuel Thoke <immanuel@matchwerk.de>
2021-08-28 13:41:49 +0200

    fix rotation of y on the wrong place

--- Sugiyama.py
+++ Sugiyama.py
@@ -3,7 +3,7 @@ import random
 from remove_cycles import remove_cycles
 from assign_levels import longest_path
 from global_sifting import GlobalSifting
-from brandes_koepf import brandes_koepf
+from brandes_koepf_copy import brandes_koepf
 import logging
 
 logger = logging.getLogger('draw graphs')
@@ -17,7 +17,7 @@ def Sugiyama(G, distance: int = 1):
     for node in G.nodes:
         x = 0
         #nx.set_node_attributes(G, { node: x }, 'x')
-        nx.set_node_attributes(G, { node: -lvl_dict[node] }, 'y')
+        nx.set_node_attributes(G, { node: lvl_dict[node] }, 'y')
         x += distance
     
     global_sifting = GlobalSifting(G)
